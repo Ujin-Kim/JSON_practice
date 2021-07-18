91b2f82ed1dd8494ac7529618baafb962146e331
Yujin Kim <best5537@gmail.com>
2021-07-18 19:57:05 +0900

    get image from json file

--- get_json_data.py
+++ get_json_data.py
@@ -1,2 +1,10 @@
 import json
 import requests
+# urllib의 urlretrieve 메소드로 이미지 다운
+import urllib.request
+
+url = requests.get("http://api.tvmaze.com/singlesearch/shows?q=Homeland&embed=episodes")
+dic = json.loads(url.text)
+
+logo = dic["image"]["medium"]
+urllib.request.urlretrieve(logo, "logo.png")
